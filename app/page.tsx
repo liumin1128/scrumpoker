c12468e408bd148e3d5915eb701e9ef22a7a525c
Min_Liu <Min_Liu@singaporeair.com.sg>
2024-06-17 17:44:07 +0800

    update: fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import useSWR from "swr";
 
-export default function Home() {
-  return <main className={styles.main}>scrumpoker</main>;
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function Index() {
+  const { data, error } = useSWR("/api/joker", fetcher);
+
+  console.log(data);
+
+  // if (error) return <div>Failed to load</div>;
+  // if (isLoading) return <div>Loading...</div>;
+
+  return <div>1</div>;
 }
