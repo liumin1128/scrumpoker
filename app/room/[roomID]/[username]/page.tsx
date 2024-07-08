8c4a636d91cd3289f2aab4f9ee3fd9b3034810e6
Min_Liu <Min_Liu@singaporeair.com.sg>
2024-07-08 19:15:43 +0800

    feat: mvp

--- app/room/[roomID]/[username]/page.tsx
+++ app/room/[roomID]/[username]/page.tsx
@@ -78,6 +78,48 @@ export interface Response {
   message?: string;
 }
 
+const calculateAverageScore = (scores: number[]) => {
+  const sum = scores.reduce((a, b) => a + b, 0);
+  const average = sum / scores.length;
+  return average.toFixed(2); // 保留两位小数
+};
+
+const findMostChosenScore = (arr: number[]): number[] => {
+  const frequencyMap: Record<number, number> = {};
+
+  // 统计每个元素的频率
+  arr.forEach((element) => {
+    frequencyMap[element] = (frequencyMap[element] || 0) + 1;
+  });
+
+  let maxFrequency = 0;
+  let mostFrequentElements: number[] = [];
+  Object.entries(frequencyMap).forEach(([element, frequency]) => {
+    if (frequency > maxFrequency) {
+      maxFrequency = frequency;
+      mostFrequentElements = [Number(element)];
+    } else if (frequency === maxFrequency) {
+      mostFrequentElements.push(Number(element));
+    }
+  });
+
+  // // 如果只有一个元素出现频率最高，返回这个元素
+  // if (mostFrequentElements.length === 1) {
+  //   return mostFrequentElements[0];
+  // }
+
+  // 如果有多个元素出现频率相同且最高，返回这些元素的数组
+  return mostFrequentElements;
+};
+
+const findMaxScore = (scores: number[]) => {
+  return Math.max(...scores);
+};
+
+const findMinScore = (scores: number[]) => {
+  return Math.min(...scores);
+};
+
 const RoomUserPage = () => {
   const router = useRouter();
   const { roomID, username } = useParams();
@@ -172,78 +214,169 @@ const RoomUserPage = () => {
     );
   };
 
+  const handleChoose = (number: number) => () => {
+    socketRef.current?.emit(
+      "voting",
+      {
+        roomID: roomID,
+        username: username,
+        voteValue: number,
+      },
+      setRoom
+    );
+  };
+
+  const me = room?.participants.find((p) => p.username === username);
+
+  if (!me) {
+    return <div>error</div>;
+  }
+
+  const voting = room?.status === "voting" && !me.hasVoted;
+
+  console.log("voting", voting);
+
+  const scores =
+    room?.status === "voted"
+      ? room?.participants.map((p) => p?.voteValue || 0) || [0]
+      : [0];
+
   return (
     <div>
-      <header className="body-font bg-gray-900 backdrop-blur-sm">
-        <div className="text-white mx-auto flex flex-wrap p-2  flex-row items-center">
-          <a className="flex title-font font-medium items-center mb-4 md:mb-0">
-            Room ID: {roomID}
-          </a>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
+      <header className="body-font bg-gray-900 backdrop-blur-sm shadow-lg shadow-slate-800/50">
+        <div className="max-h-16 text-white mx-auto flex flex-wrap p-2 flex-row items-center justify-between content-around">
+          <div>
+            <h1 className="title-font font-medium  text-xl text-slate-200">
+              Room ID: {roomID}
+            </h1>
+            <h2 className="title-font font-medium text-xs text-slate-200">
+              Username: {username}
+            </h2>
+          </div>
 
           {room?.status !== "voting" && (
             <button
-              className="text-gray-900 inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 bg-cyan-500 shadow-lg shadow-cyan-500/50 ..."
+              className="w-32 text-center text-white inline-flex items-center justify-center  border-0 py-2 px-3 focus:outline-none hover:bg-green-600 rounded text-base bg-green-500 shadow-lg "
               onClick={handleStartVoting}
             >
-              start voting
+              Start Voting
             </button>
           )}
+
           {room?.status === "voting" && (
             <button
-              className="text-gray-900 inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 bg-cyan-500 shadow-lg shadow-cyan-500/50 ..."
+              className="w-32 text-center text-white inline-flex items-center justify-center border-0 py-2 px-3 focus:outline-none hover:bg-red-600 rounded text-base bg-red-500 shadow-lg "
               onClick={handleEndVoting}
             >
-              end voting
+              End Voting
             </button>
           )}
-          <button
-            className="text-gray-900 inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 bg-cyan-500 shadow-lg shadow-cyan-500/50 ..."
-            onClick={handleVoting}
-          >
-            voting
-          </button>
         </div>
       </header>
 
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
-            {room?.participants.map((participant) => {
-              return (
-                <div
-                  key={participant.username}
-                  className={`p-4 flip-card rounded-lg ${
-                    room?.status === "voting" && participant.hasVoted
-                      ? "flipped"
-                      : ""
-                  }`}
-                >
-                  <div
-                    className={`bg-teal-800 shadow-lg shadow-slate-800/50 rounded-lg card-bg card flip-card-inner `}
-                  >
-                    <div className="flip-card-front rounded-lg">
-                      {/* 卡牌正面内容 */}
-                      <h1 className="font-bold title-font text-lg">
-                        {room?.status === "voted" && participant.voteValue}
-                        {room?.status === "voting" ? "voting" : ""}
-                      </h1>
-                    </div>
-                    <div className="flip-card-back rounded-lg">
-                      {/* 卡牌背面内容 */}
-                      <h1></h1>
-                    </div>
-                  </div>
-                  <h3 className="pt-2 text-center tracking-widest text-white text-xs font-medium title-font">
-                    {participant.username}
-                  </h3>
-                </div>
-              );
-            })}
+      <section className="text-gray-600 mt-8 sm:mt-24 body-font ">
+        <div className="container px-5 py-2 mx-auto">
+          <div className="flex flex-wrap -m-4 text-center">
+            <div className="p-4 sm:w-1/4 w-1/2">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-slate-200">
+                {calculateAverageScore(scores)}
+              </h2>
+              <p className="leading-relaxed">Avg. score</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-slate-200">
+                {findMostChosenScore(scores).join(", ")}
+              </h2>
+              <p className="leading-relaxed">Most chosen</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-slate-200">
+                {findMaxScore(scores)}
+              </h2>
+              <p className="leading-relaxed">Max. score</p>
+            </div>
+            <div className="p-4 sm:w-1/4 w-1/2">
+              <h2 className="title-font font-medium sm:text-4xl text-3xl text-slate-200">
+                {findMinScore(scores)}
+              </h2>
+              <p className="leading-relaxed">Min. score</p>
+            </div>
           </div>
         </div>
       </section>
 
+      {voting ? (
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-8 sm:py-24 mx-auto">
+            <div className="justify-center grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 place-content-center">
+              {[0, 1, , 2, 3, 5, 8, 13, 21].map((i) => {
+                return (
+                  <button
+                    onClick={handleChoose(i as number)}
+                    key={i}
+                    className={`p-1 sm:p-4 flip-card rounded-lg`}
+                  >
+                    <div
+                      className={`bg-teal-800 shadow-lg shadow-slate-800/50 rounded-lg card-bg card flip-card-inner `}
+                    >
+                      <div className="flip-card-front rounded-lg">
+                        <h1 className="font-bold title-font text-lg">{i}</h1>
+                      </div>
+                    </div>
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      ) : (
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-8 sm:py-24 mx-auto">
+            <div className="justify-center grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 place-content-center">
+              {room?.participants.map((participant) => {
+                return (
+                  <button
+                    key={participant.username}
+                    className={`p-1 sm:p-4 flip-card rounded-lg ${
+                      room?.status === "voting" && participant.hasVoted
+                        ? "flipped"
+                        : ""
+                    }`}
+                  >
+                    <div
+                      className={`bg-teal-800 shadow-lg shadow-slate-800/50 rounded-lg card-bg card flip-card-inner `}
+                    >
+                      <div className="flip-card-front rounded-lg">
+                        {/* 卡牌正面内容 */}
+                        <h1 className="font-bold title-font  title-font sm:text-4xl text-3xl">
+                          {room?.status === "voted" && participant.voteValue}
+                        </h1>
+                        <span className="font-normal title-font  title-font sm:text-xs text-xs">
+                          {room?.status === "voting" ? "votin" : ""}
+                        </span>
+                      </div>
+                      <div className="flip-card-back rounded-lg">
+                        {/* 卡牌背面内容 */}
+                        <h1></h1>
+                      </div>
+                    </div>
+                    <h3
+                      className={`pt-2 text-center tracking-widest text-white text-xs font-medium title-font ${
+                        participant?.clientIDs?.length === 0
+                          ? "offline"
+                          : "online"
+                      }`}
+                    >
+                      {participant.username}
+                      <div className="status-icon"></div>
+                    </h3>
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      )}
       {/* <pre>{JSON.stringify(room, null, 2)}</pre> */}
     </div>
   );
